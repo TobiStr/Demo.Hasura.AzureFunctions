e75c3c3ebb2c86360c8ce51b9f774dcdac3cef26
Tobias Streng <streng@PZS.de>
2022-02-03 10:31:44 +0100

    Extract Model to Separate file

--- src/Demo.Hasura.AzureFunctions/HasuraDemoAction.cs
+++ src/Demo.Hasura.AzureFunctions/HasuraDemoAction.cs
@@ -39,8 +39,3 @@ public class HasuraDemoAction
     }
 }
 
-internal record HasuraDemoRequest(HasuraDemoInput Input);
-
-internal record HasuraDemoInput(string Username, string Message);
-
-internal record Result(bool Success, string Message);
---
+++ src/Demo.Hasura.AzureFunctions/Model.cs
@@ -0,0 +1,7 @@
+﻿namespace Demo.Hasura.AzureFunctions;
+
+internal record HasuraDemoRequest(HasuraDemoInput Input);
+
+internal record HasuraDemoInput(string Username, string Message);
+
+internal record Result(bool Success, string Message);
